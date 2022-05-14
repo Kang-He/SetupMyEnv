a971f5ba6a3eb8b01ec052efae8a10e185592b1a
hekang <363352333@qq.com>
2022-05-14 10:14:39 -0500

    更新git_config的错误

--- git_config.sh
+++ git_config.sh
@@ -1,9 +1,9 @@
 #变量配置
 envPath="/users/zhang56/"
-projectName="SetupMyenv"
+projectName="SetupMyEnv"
 
 ###############################开始################################
-remoteURL="git@github.com:Kang-He/$projectName"
+remoteURL="git@github.com:Kang-He/${projectName}.git"
 mkdir -p $envPath
 cd $envPath
 #建立项目文件夹
@@ -14,5 +14,6 @@ git config --global user.name "hekang"
 git config --global user.email "363352333@qq.com"
 #初始化项目
 git init
-git remote add origin $remoteURL
+echo "set-url $remoteURL"
+git remote set-url origin $remoteURL
 
