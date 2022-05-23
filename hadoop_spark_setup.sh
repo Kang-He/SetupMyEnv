109aa76259b1820a2cba8131ece58396f572956a
hekang <363352333@qq.com>
2022-05-23 04:48:25 -0500

    修改安装脚本

--- hadoop_spark_setup.sh
+++ hadoop_spark_setup.sh
@@ -4,41 +4,18 @@ scriptsPath=${HOME}/SetMyEnv
 ###############################开始################################
 #安装java、scala、hadoop、spark
 
-cd $envPath
-##安装java
-wget https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.tar.gz
-tar -xzvf jdk-18_linux-x64_bin.tar.gz
-
-##安装scala
-wget https://downloads.lightbend.com/scala/2.12.15/scala-2.12.15.tgz
-tar -xzvf scala-2.12.15.tgz
-
-##安装hadoop
-wget https://dlcdn.apache.org/hadoop/common/hadoop-3.2.3/hadoop-3.2.3.tar.gz
-tar -zxvf hadoop-3.2.3.tar.gz
-
-##安装spark
-wget https://downloads.apache.org/spark/spark-3.1.3/spark-3.1.3-bin-hadoop3.2.tgz
-tar -xzvf spark-3.1.3-bin-hadoop3.2.tgz
-
+/local/repository/Utils.sh 10 CMD "$scriptsPath/download_tar.sh"
 
 
 #配置环境变量
 cd $scriptsPath
-cat ./addpath >> ${HOME}/.bash_profile
+/local/repository/Utils.sh 10 CMD "cat $scriptsPath/addpath >> ${HOME}/.bash_profile"
 
 
 #配置hadoop和spark的配置文件
 
 ##配置hadoop
-cp ./hadoop_config/* ${envPath}/hadoop-3.2.3/etc/hadoop/
+/local/repository/Utils.sh 10 CMD "cp $scriptsPath/hadoop_config/* ${envPath}/hadoop-3.2.3/etc/hadoop/"
 
 ##配置spark
-cp ./spark_config/* ${envPath}/spark-3.1.3-bin-hadoop3.2/conf/ 
-
-
-#将程序文件分发到其他节点
-###创建文件
-/local/repository/Utils.sh 3 CMD "mkdir -p $envPath"
-###将程序文件全部分发
-/local/repository/Utils.sh 3 PUT " $envPath/*"  $envPath/
+/local/repository/Utils.sh 10 CMD "cp $scriptsPath/spark_config/* ${envPath}/spark-3.1.3-bin-hadoop3.2/conf/" 
