63a9686f876b437d4824228fcf270842d6d8c403
hekang <363352333@qq.com>
2022-05-24 03:58:22 -0500

    -

--- hadoop_spark_setup.sh
+++ hadoop_spark_setup.sh
@@ -5,12 +5,14 @@ scriptsPath=${HOME}/SetupMyEnv
 #安装java、scala、hadoop、spark
 
 /local/repository/Utils.sh 10 CMD "$scriptsPath/download_tar.sh"
-
+echo "软件安装完成"
+sleep 8
 
 #配置环境变量
 cd $scriptsPath
 /local/repository/Utils.sh 10 CMD "cat $scriptsPath/addpath >> ${HOME}/.bash_profile"
-
+echo "环境变量配置完成"
+sleep 5
 
 #配置hadoop和spark的配置文件
 
@@ -19,3 +21,4 @@ cd $scriptsPath
 
 ##配置spark
 /local/repository/Utils.sh 10 CMD "cp $scriptsPath/spark_config/* ${envPath}/spark-3.1.3-bin-hadoop3.2/conf/" 
+echo "hadoop spark初始配置完成"
